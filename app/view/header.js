5cd77154de5423896ef1c582001c1cb2f6a940d1
Giscomaps <63242716+Giscomaps@users.noreply.github.com>
2020-05-21 15:02:18 -0500

    Update header.js

--- app/view/header.js
+++ app/view/header.js
@@ -2,7 +2,7 @@ function Header(){
     return (
         <nav className='title'>
             <h1>Multilabel classifier</h1>
-            <span className='ramaTwitter'><a href='https://twitter.com/ramgendeploy' target='_blank'>@ramgendeploy</a></span>
+            <span className='ramaTwitter'><a href='www.unal.edu.co' target='_blank'>jballes@unal.edu.co</a></span>
         </nav>
     )
 }
